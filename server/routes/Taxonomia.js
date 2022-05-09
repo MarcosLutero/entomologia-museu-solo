bd929bc638c0aa2e56a95806d92414f6053bca04
Denis Junior <denis_jr2001@hotmail.com>
2022-05-09 17:10:08 -0300

    option taxonomia added

--- server/routes/Taxonomia.js
+++ server/routes/Taxonomia.js
@@ -1,18 +1,27 @@
 const express = require("express");
 const router = express.Router();
-const {Taxonomia} = require("../models")
+const { Taxonomia } = require("../models");
 
 router.get("/", async (req, res) => {
-    const listOfTaxonomia = await Taxonomia.findAll()
-    res.json(listOfTaxonomia)
+  const listOfTaxonomia = await Taxonomia.findAll();
+  res.json(listOfTaxonomia);
+});
 
+router.get("/options", async (req, res) => {
+  await Taxonomia.findAll({
+    attributes: [
+      ["id", "value"],
+      ["nome", "label"],
+    ],
+  }).then((options) => {
+    res.json(options);
+  });
 });
 
 router.post("/", async (req, res) => {
-    const taxonomia = req.body
-    await Taxonomia.create(taxonomia)
-    res.json(taxonomia)
-})
-
+  const taxonomia = req.body;
+  await Taxonomia.create(taxonomia);
+  res.json(taxonomia);
+});
 
 module.exports = router;
