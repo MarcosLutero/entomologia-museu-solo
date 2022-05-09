4f01307c611fb6850102fe276bb1d8adcd231e8e
Denis Junior <denis_jr2001@hotmail.com>
2022-05-09 17:12:29 -0300

    bootstrap nav in modal added

--- client/src/Components/AdminHomePage/AdminHomePage.jsx
+++ client/src/Components/AdminHomePage/AdminHomePage.jsx
@@ -1,10 +1,7 @@
-import React, { useState } from "react";
-import Modal from "react-modal";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import Logo from "../../assets/img/Brasao_ufra.png";
 import FormAddCharacteristics from "../FormAddCharacteristics/FormAddCharacteristics";
-
-Modal.setAppElement("#root");
 
 const AdminHomePage = () => {
   const navigate = useNavigate();
@@ -47,297 +44,178 @@ const AdminHomePage = () => {
         </div>
       </nav>
 
+      <div className="container bg-light d-flex flex-column justify-content-center w-25 mt-5">
+        <h3 className="text-center mt-3 mb-4">Clique na opção desejada</h3>
 
-                <div className="container bg-light d-flex flex-column justify-content-center w-25 mt-5">
-                  <h3 className="text-center mt-3 mb-4">Clique na opção desejada</h3>
-                  
-                  <button
-                    type="button"
-                    className="btn btn-outline-primary w-auto mx-auto mb-3"
-                    data-bs-toggle="modal"
-                    data-bs-target="#adicionar-classe"
+        <button
+          type="button"
+          className="btn btn-outline-primary w-auto mx-auto mb-3"
+          data-bs-toggle="modal"
+          data-bs-target="#adicionar-classe"
+        >
+          Adicionar Classe
+        </button>
+        <div
+          className="modal fade"
+          id="adicionar-classe"
+          tabIndex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-body">
+                <ul
+                  className="nav nav-pills mb-3"
+                  id="pills-tab"
+                  role="tablist"
+                >
+                  <li className="nav-item" role="presentation">
+                    <button
+                      className="nav-link active"
+                      id="pills-home-tab"
+                      data-bs-toggle="pill"
+                      data-bs-target="#pills-home"
+                      type="button"
+                      role="tab"
+                      aria-controls="pills-home"
+                      aria-selected="true"
+                    >
+                      Adicionar Taxonomia
+                    </button>
+                  </li>
+                  <li className="nav-item" role="presentation">
+                    <button
+                      className="nav-link"
+                      id="pills-profile-tab"
+                      data-bs-toggle="pill"
+                      data-bs-target="#pills-profile"
+                      type="button"
+                      role="tab"
+                      aria-controls="pills-profile"
+                      aria-selected="false"
+                    >
+                      Adicionar Classificação
+                    </button>
+                  </li>
+                </ul>
+                <div className="tab-content" id="pills-tabContent">
+                  <div
+                    className="tab-pane fade show active"
+                    id="pills-home"
+                    role="tabpanel"
+                    aria-labelledby="pills-home-tab"
                   >
-                    Adicionar Classe
-                  </button>
-                  <div className="modal fade"
-                    id="adicionar-classe"
-                    tabindex="-1"
-                    aria-labelledby="exampleModalLabel"
-                    aria-hidden="true"
-                  >
-                    <div className="modal-dialog">
-                      <div className="modal-content">
-                        <div className="modal-header">
-                          <h5 className="modal-title" id="exampleModalLabel">
-                            Adicionar Classe
-                          </h5>
-                          <button
-                            type="button"
-                            className="btn-close"
-                            data-bs-dismiss="modal"
-                            aria-label="Close"
-                          ></button>
-                        </div>
-                        <div className="modal-body">
-
-                            <FormAddCharacteristics content={"testando"}/>
-
-                        </div>
-                        <div className="modal-footer">
-                          <button
-                            type="button"
-                            className="btn btn-secondary"
-                            data-bs-dismiss="modal"
-                          >
-                            Fechar
-                          </button>
-                          <button type="button" className="btn btn-primary">
-                            Salvar
-                          </button>
-                        </div>
-                      </div>
-                    </div>
+                    <FormAddCharacteristics/>
                   </div>
-                  <button
-                    type="button"
-                    className="btn btn-outline-primary w-auto mx-auto mb-3"
-                    data-bs-toggle="modal"
-                    data-bs-target="#adicionar-familia"
+                  <div
+                    className="tab-pane fade"
+                    id="pills-profile"
+                    role="tabpanel"
+                    aria-labelledby="pills-profile-tab"
                   >
-                    Adicionar Família
-                  </button>
-                  <div className="modal fade"
-                    id="adicionar-familia"
-                    tabindex="-1"
-                    aria-labelledby="exampleModalLabel"
-                    aria-hidden="true"
-                  >
-                    <div className="modal-dialog">
-                      <div className="modal-content">
-                        <div className="modal-header">
-                          <h5 className="modal-title" id="exampleModalLabel">
-                            Adicionar Família
-                          </h5>
-                          <button
-                            type="button"
-                            className="btn-close"
-                            data-bs-dismiss="modal"
-                            aria-label="Close"
-                          ></button>
-                        </div>
-                        <div className="modal-body">
-
-                            <FormAddCharacteristics content={"testando"}/>
-
-                        </div>
-                        <div className="modal-footer">
-                          <button
-                            type="button"
-                            className="btn btn-secondary"
-                            data-bs-dismiss="modal"
-                          >
-                            Fechar
-                          </button>
-                          <button type="button" className="btn btn-primary">
-                            Salvar
-                          </button>
-                        </div>
-                      </div>
-                    </div>
+                    <FormAddCharacteristics/>
                   </div>
-                  <button
-                    type="button"
-                    className="btn btn-outline-primary w-auto mx-auto mb-3"
-                    data-bs-toggle="modal"
-                    data-bs-target="#adicionar-filo"
-                  >
-                    Adicionar Filo
-                  </button>
-                  <div className="modal fade"
-                    id="adicionar-filo"
-                    tabindex="-1"
-                    aria-labelledby="exampleModalLabel"
-                    aria-hidden="true"
-                  >
-                    <div className="modal-dialog">
-                      <div className="modal-content">
-                        <div className="modal-header">
-                          <h5 className="modal-title" id="exampleModalLabel">
-                            Adicionar Filo
-                          </h5>
-                          <button
-                            type="button"
-                            className="btn-close"
-                            data-bs-dismiss="modal"
-                            aria-label="Close"
-                          ></button>
-                        </div>
-                        <div className="modal-body">
-
-                            <FormAddCharacteristics content={"testando"}/>
-
-                        </div>
-                        <div className="modal-footer">
-                          <button
-                            type="button"
-                            className="btn btn-secondary"
-                            data-bs-dismiss="modal"
-                          >
-                            Fechar
-                          </button>
-                          <button type="button" className="btn btn-primary">
-                            Salvar
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <button
-                    type="button"
-                    className="btn btn-outline-primary w-auto mx-auto mb-3"
-                    data-bs-toggle="modal"
-                    data-bs-target="#adicionar-ordem"
-                  >
-                    Adicionar Ordem
-                  </button>
-                  <div className="modal fade"
-                    id="adicionar-ordem"
-                    tabindex="-1"
-                    aria-labelledby="exampleModalLabel"
-                    aria-hidden="true"
-                  >
-                    <div className="modal-dialog">
-                      <div className="modal-content">
-                        <div className="modal-header">
-                          <h5 className="modal-title" id="exampleModalLabel">
-                            Adicionar Ordem
-                          </h5>
-                          <button
-                            type="button"
-                            className="btn-close"
-                            data-bs-dismiss="modal"
-                            aria-label="Close"
-                          ></button>
-                        </div>
-                        <div className="modal-body">
-
-                            <FormAddCharacteristics content={"testando"}/>
-
-                        </div>
-                        <div className="modal-footer">
-                          <button
-                            type="button"
-                            className="btn btn-secondary"
-                            data-bs-dismiss="modal"
-                          >
-                            Fechar
-                          </button>
-                          <button type="button" className="btn btn-primary">
-                            Salvar
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  
-                  
-                  <button
-                    type="button"
-                    className="btn btn-outline-primary w-auto mx-auto mb-3"
-                    data-bs-toggle="modal"
-                    data-bs-target="#adicionar-genero"
-                  >
-                    Adicionar Gênero
-                  </button>
-                  <div className="modal fade"
-                    id="adicionar-genero"
-                    tabindex="-1"
-                    aria-labelledby="exampleModalLabel"
-                    aria-hidden="true"
-                  >
-                    <div className="modal-dialog">
-                      <div className="modal-content">
-                        <div className="modal-header">
-                          <h5 className="modal-title" id="exampleModalLabel">
-                            Adicionar Gênero
-                          </h5>
-                          <button
-                            type="button"
-                            className="btn-close"
-                            data-bs-dismiss="modal"
-                            aria-label="Close"
-                          ></button>
-                        </div>
-                        <div className="modal-body">
-
-                            <FormAddCharacteristics content={"content second Button"}/>
-
-                        </div>
-                        <div className="modal-footer">
-                          <button
-                            type="button"
-                            className="btn btn-secondary"
-                            data-bs-dismiss="modal"
-                          >
-                            Fechar
-                          </button>
-                          <button type="button" className="btn btn-primary">
-                            Salvar
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <button
-                    type="button"
-                    className="btn btn-outline-success w-auto mx-auto mb-5"
-                    data-bs-toggle="modal"
-                    data-bs-target="#adicionar-inseto"
-                  >
-                   - - - - - Adicionar Inseto - - - - -
-                  </button>
-                  <div className="modal fade"
-                    id="adicionar-inseto"
-                    tabindex="-1"
-                    aria-labelledby="exampleModalLabel"
-                    aria-hidden="true"
-                  >
-                    <div className="modal-dialog">
-                      <div className="modal-content">
-                        <div className="modal-header">
-                          <h5 className="modal-title" id="exampleModalLabel">
-                            Adicionar Inseto
-                          </h5>
-                          <button
-                            type="button"
-                            className="btn-close"
-                            data-bs-dismiss="modal"
-                            aria-label="Close"
-                          ></button>
-                        </div>
-                        <div className="modal-body">
-
-                            <FormAddCharacteristics content={"content second Button"}/>
-
-                        </div>
-                        <div className="modal-footer">
-                          <button
-                            type="button"
-                            className="btn btn-secondary"
-                            data-bs-dismiss="modal"
-                          >
-                            Fechar
-                          </button>
-                          <button type="button" className="btn btn-primary">
-                            Salvar
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-
                 </div>
+              </div>
+            </div>
+          </div>
+        </div>
 
+        <button
+          type="button"
+          className="btn btn-outline-primary w-auto mx-auto mb-3"
+          data-bs-toggle="modal"
+          data-bs-target="#adicionar-familia"
+        >
+          Adicionar Família
+        </button>
+        <div
+          className="modal fade"
+          id="adicionar-familia"
+          tabIndex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">
+                  Adicionar Família
+                </h5>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="modal-body">
+                <FormAddCharacteristics content={"testando"} />
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                >
+                  Fechar
+                </button>
+                <button type="button" className="btn btn-primary">
+                  Salvar
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <button
+          type="button"
+          className="btn btn-outline-success w-auto mx-auto mb-5"
+          data-bs-toggle="modal"
+          data-bs-target="#adicionar-inseto"
+        >
+          - - - - - Adicionar Inseto - - - - -
+        </button>
+        <div
+          className="modal fade"
+          id="adicionar-inseto"
+          tabIndex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">
+                  Adicionar Inseto
+                </h5>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="modal-body">
+                <FormAddCharacteristics content={"content second Button"} />
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                >
+                  Fechar
+                </button>
+                <button type="button" className="btn btn-primary">
+                  Salvar
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
