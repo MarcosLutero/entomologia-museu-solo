f72ce861fd74935ebb505fb1e6e038800f991277
Denis Junior <denis_jr2001@hotmail.com>
2022-05-09 17:09:50 -0300

    only formatting

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const app = express();
-const cors = require('cors')
+const cors = require("cors");
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 const db = require("./models");
 
 //Routes
-const taxonomiaRouter = require('./routes/Taxonomia')
-app.use("/taxonomia", taxonomiaRouter)
+const taxonomiaRouter = require("./routes/Taxonomia");
+app.use("/taxonomia", taxonomiaRouter);
 
 db.sequelize.sync().then(() => {
   app.listen(3001, () => {
