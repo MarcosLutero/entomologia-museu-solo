45e712245c0fe38d243346ff151e60d033767908
Denis <denis_jr2001@hotmail.com>
2022-05-10 00:52:22 -0300

    added card for bug informations

--- client/src/Components/HomePage/HomePage.jsx
+++ client/src/Components/HomePage/HomePage.jsx
@@ -3,6 +3,8 @@ import FormSearchBug from "../FormSearchBug/FormSearchBug";
 import NavMain from "../NavMain/NavMain";
 import BugInformation from "../BugInformation/BugInformation";
 
+import bug from "../../assets/img/louvaDeus.jpg";
+
 const HomePage = ({
   handleSubmit,
   handleInputChange,
@@ -23,6 +25,47 @@ const HomePage = ({
         setInputValue={setInputValue}
       />
       {validationData ? <BugInformation /> : <div></div>}
+
+      <div className="container mt-4">
+        <div class="row row-cols-1 row-cols-md-5">
+          <div class="col" onClick={() => {
+              console.log("oi")
+            }}>
+            <div class="card">
+              <img src={bug} class="card-img-top" alt="imagem do inseto" title="inseto"/>
+              <div class="card-body">
+                <h5 class="card-title"><b>Besourinho</b></h5>
+                <p class="card-text">
+                  <b>Habitat:</b> habitat xyz
+                </p>
+                <p class="card-text">
+                  <b>Asa:</b> asa xyz
+                </p>
+                <p class="card-text">
+                  <b>Pernas:</b> pernas xyz
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              <img src={bug} class="card-img-top" alt="imagem do inseto" title="inseto"/>
+              <div class="card-body">
+                <h5 class="card-title"><b>Besourinho</b></h5>
+                <p class="card-text">
+                  <b>Habitat:</b> habitat xyz
+                </p>
+                <p class="card-text">
+                  <b>Asa:</b> asa xyz
+                </p>
+                <p class="card-text">
+                  <b>Pernas:</b> pernas xyz
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
