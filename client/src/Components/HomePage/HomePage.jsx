39e66186cb26eb34c25d45f9dccc906546dd8528
Denis Junior <denis_jr2001@hotmail.com>
2022-05-10 16:54:31 -0300

    bugInformation removed and ListEspecies added

--- client/src/Components/HomePage/HomePage.jsx
+++ client/src/Components/HomePage/HomePage.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import FormSearchBug from "../FormSearchBug/FormSearchBug";
 import NavMain from "../NavMain/NavMain";
-import BugInformation from "../BugInformation/BugInformation";
 
-import bug from "../../assets/img/louvaDeus.jpg";
+import ListEspecies from "../ListEspecies/ListEspecies";
 
 const HomePage = ({
   handleSubmit,
@@ -24,48 +23,8 @@ const HomePage = ({
         handleInputChange={handleInputChange}
         setInputValue={setInputValue}
       />
-      {validationData ? <BugInformation /> : <div></div>}
+      {validationData ? <ListEspecies data={data} /> : <div></div>}
 
-      <div className="container mt-4">
-        <div class="row row-cols-1 row-cols-md-5">
-          <div class="col" onClick={() => {
-              console.log("oi")
-            }}>
-            <div class="card">
-              <img src={bug} class="card-img-top" alt="imagem do inseto" title="inseto"/>
-              <div class="card-body">
-                <h5 class="card-title"><b>Besourinho</b></h5>
-                <p class="card-text">
-                  <b>Habitat:</b> habitat xyz
-                </p>
-                <p class="card-text">
-                  <b>Asa:</b> asa xyz
-                </p>
-                <p class="card-text">
-                  <b>Pernas:</b> pernas xyz
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="col">
-            <div class="card">
-              <img src={bug} class="card-img-top" alt="imagem do inseto" title="inseto"/>
-              <div class="card-body">
-                <h5 class="card-title"><b>Besourinho</b></h5>
-                <p class="card-text">
-                  <b>Habitat:</b> habitat xyz
-                </p>
-                <p class="card-text">
-                  <b>Asa:</b> asa xyz
-                </p>
-                <p class="card-text">
-                  <b>Pernas:</b> pernas xyz
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
     </>
   );
 };
