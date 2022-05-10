201ff21400e9df43b3ee9d05176ab7fe9023ba96
Denis Junior <denis_jr2001@hotmail.com>
2022-05-10 16:53:08 -0300

    bug information local changed and new features

--- client/src/Components/BugInformation/BugInformation.jsx
+++ client/src/Components/BugInformation/BugInformation.jsx
@@ -1,31 +1,35 @@
 import React from "react";
 import BugPic from "../../assets/img/louvaDeus.jpg";
+import NavMain from "../NavMain/NavMain";
 
-const BugInformation = () => {
+const BugInformation = ({dataInsect}) => {
   return (
     <>
-      <div className="card mb-3 w-75 mt-4 mx-auto bg-secondary">
-      <h3 className="text-white text-center mt-3">Besourinho</h3>
-        <img src={BugPic} className="card-img-top" alt="inseto" title="inseto" />
+      <NavMain/>
+
+      <div className="card mb-3 w-50 mt-4 mx-auto bg-secondary">
+      <h3 className="text-white text-center mt-3">{dataInsect.nomeVulgar}</h3>
+        <img src={BugPic} className="card-img-top" alt={dataInsect.nomeVulgar} title={dataInsect.nomeVulgar} />
         <div className="card-body">
           <h5 className="card-title text-center text-white mt-1">
             Características Gerais
           </h5>
         <div className="row row-cols-md-2 mx-auto">
           <ul className="list-group mb-3">
-            <li className="list-group-item"><b>Abdomen:</b> Tipo abdomen xyz</li>
-            <li className="list-group-item"><b>Antenas:</b> Tipo antena xyz</li>
-            <li className="list-group-item"><b>Aparelho Bucal:</b> Tipo Aparelho Bucal xyz</li>
-            <li className="list-group-item"><b>Asas:</b> Tipo asa xyz</li>
-            <li className="list-group-item"><b>Habitat:</b> Casa do Caralho</li>
-            <li className="list-group-item"><b>Pernas:</b> Tipo perna xyz</li>
+            <li className="list-group-item"><b>Abdomen:</b> {dataInsect.abdomen}</li>
+            <li className="list-group-item"><b>Antenas:</b> {dataInsect.antena}</li>
+            <li className="list-group-item"><b>Aparelho Bucal:</b> {dataInsect.aparelhoBucal}</li>
+            <li className="list-group-item"><b>Asas:</b> {dataInsect.asa}</li>
+            <li className="list-group-item"><b>Habitat:</b>{dataInsect.habitat}</li>
+            <li className="list-group-item"><b>Pernas:</b> {dataInsect.perna}</li>
           </ul>
           <ul className="list-group">
-            <li className="list-group-item"><b>Filo:</b> Arthropoda</li>
-            <li className="list-group-item"><b>Classe:</b> Insecta</li>
-            <li className="list-group-item"><b>Ordem:</b> Hymenoptera</li>
-            <li className="list-group-item"><b>Família:</b> Pompilidae</li>
-            <li className="list-group-item"><b>Gênero:</b> Pepsis</li>
+            <li className="list-group-item"><b>Filo:</b> {dataInsect.filo}</li>
+            <li className="list-group-item"><b>Classe:</b> {dataInsect.classe}</li>
+            <li className="list-group-item"><b>Ordem:</b> {dataInsect.ordem}</li>
+            <li className="list-group-item"><b>Família:</b> {dataInsect.familia}</li>
+            <li className="list-group-item"><b>Gênero:</b> {dataInsect.genero}</li>
+            <li className="list-group-item"><b>Curiosidade:</b> {dataInsect.curiosidade}</li>
           </ul>
 
         </div>
