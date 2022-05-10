841e326ac0e4f7ee7ca56bd6e348c87b084b0b4c
Denis Junior <denis_jr2001@hotmail.com>
2022-05-10 16:56:03 -0300

    list species grid created

---
+++ client/src/Components/ListEspecies/ListEspecies.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import bug from "../../assets/img/louvaDeus.jpg";
+
+
+const ListEspecies = ({data}) => {
+    
+    let navigate = useNavigate()
+
+    useEffect(() => {
+        console.log(data)
+    })
+    return ( <>
+        
+      <div className="container mt-4">
+        <div className="row row-cols-1 row-cols-md-3">
+            {
+                data.map((inseto, index) => {
+                    return(
+          <div key={index} className="col mb-5" onClick={() => {
+              navigate(`/${inseto.nomeVulgar}`)
+            }}>
+            <div className="card">
+              <img src={bug} className="card-img-top" alt="imagem do inseto" title="inseto"/>
+              <div className="card-body">
+                <h5 className="card-title"><b>{inseto.nomeVulgar}</b></h5>
+                <p className="card-text">
+                  <b>Habitat:</b> {inseto.habitat}
+                </p>
+                <p className="card-text">
+                  <b>Asa:</b> {inseto.asa}
+                </p>
+                <p className="card-text">
+                  <b>Pernas:</b> {inseto.perna}
+                </p>
+              </div>
+            </div>
+          </div>)
+                })
+            }
+
+        </div>
+      </div>
+    
+    </> );
+}
+ 
+export default ListEspecies;
