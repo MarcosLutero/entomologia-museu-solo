f6a68fe1b7d4e2e9f5a188e98f0417c7c2dda2a5
Denis Junior <denis_jr2001@hotmail.com>
2022-05-12 21:15:05 -0300

    using api now

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,90 +5,25 @@ import AdminHomePage from "./Components/AdminHomePage/AdminHomePage";
 import HomePage from "./Components/HomePage/HomePage";
 import CuriosidadesPage from "./Components/CuriosidadesPage/CuriosidadesPage";
 import InsetoDetails from "./Components/InsetoDetails/InsetoDetails";
-import NavMain from "./Components/NavMain/NavMain";
 import LoginPage from "./Components/LoginPage/LoginPage";
 
 const App = () => {
   useEffect(() => {
-    axios.get("http://localhost:3001/taxonomia").then((response) => {
+    axios.get("https://api-museu-entomologiaufra.herokuapp.com/abdomens").then((response) => {
       console.log(response.data);
       setData(response.data);
     });
+
+    console.log(data)
+
   }, []);
 
   // value from user
   const [inputValue, setInputValue] = useState("");
 
   //data from backend
-  const [data, setData] = useState([
-    {
-      nomeVulgar: "Louva Deus",
-      abdomen: "abdomen louva Deus",
-      asa: "asa louva Deus",
-      perna: "perna louva Deus",
-      aparelhoBucal: "aparelho bucal louva Deus",
-      antena: "antena louva Deus",
-      metamorfose: "metamorfose louva Deus",
-      comportamento: "comportamento louva Deus",
-      habitat: "habitat louva Deus",
-      filo: "filo louva Deus",
-      classe: "Insecta louva Deus",
-      ordem: "ordem louva Deus",
-      familia: "familia louva Deus",
-      genero: "genero louva Deus",
-      curiosidade: "é um inseto que curte louvar a Deus",
-    },
-    {
-      nomeVulgar: "Besourinho",
-      abdomen: "abdomen Besourinho",
-      asa: "asa Besourinho",
-      perna: "perna Besourinho",
-      aparelhoBucal: "aparelho bucal Besourinho",
-      antena: "antena Besourinho",
-      metamorfose: "metamorfose Besourinho",
-      comportamento: "comportamento Besourinho",
-      habitat: "habitat Besourinho",
-      filo: "filo Besourinho",
-      classe: "Insecta Besourinho",
-      ordem: "ordem Besourinho",
-      familia: "familia Besourinho",
-      genero: "genero Besourinho",
-      curiosidade:
-        "inseto de boa com a vida que curte batalhar com outros insetos",
-    },
-    {
-      nomeVulgar: "Cavalo do Cao",
-      abdomen: "abdomen Cavalo do Cao",
-      asa: "asa Cavalo do Cao",
-      perna: "perna Cavalo do Cao",
-      aparelhoBucal: "aparelho bucal Cavalo do Cao",
-      antena: "antena Cavalo do Cao",
-      metamorfose: "metamorfose Cavalo do Cao",
-      comportamento: "comportamento Cavalo do Cao",
-      habitat: "habitat Cavalo do Cao",
-      filo: "filo Cavalo do Cao",
-      classe: "Insecta Cavalo do Cao",
-      ordem: "ordem Cavalo do Cao",
-      familia: "familia Cavalo do Cao",
-      genero: "genero Cavalo do Cao",
-      curiosidade:
-        "inseto desgraçado, não é atoa que o nome é cavalo do cão, capaz de matar um boi",
-    },
-  ]);
+  const [data, setData] = useState([]);
 
-  //function for make a request for backend
-  // const getData = () => {
-  //   axios
-  //     .get("/urlForGetDataFromBug")
-  //     .then( (response) => {
-  //       // manipula o sucesso da requisição
-  //       setData(response);
-  //     })
-  //     .catch( (error) => {
-  //       // manipula erros da requisição
-  //       console.error(error);
-  //     })
-  // };
 
   //catch the value from the form that contain the input user
   const handleSubmit = (e) => {
@@ -110,6 +45,13 @@ const App = () => {
 
   return (
     <BrowserRouter>
+      
+      {/* {data.map((element, index) => {
+        return (
+          <h1 key={index} >{element.nome_abdomen}</h1>
+        )
+      })} */}
+      
       <Routes>
         <Route
           path="/"
