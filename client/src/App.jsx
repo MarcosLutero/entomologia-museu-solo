086cee73064c9d15dc73360ca81171330497be87
Denis <denis_jr2001@hotmail.com>
2022-05-15 20:50:24 -0300

    characteristics page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,23 +6,78 @@ import HomePage from "./Components/HomePage/HomePage";
 import CuriosidadesPage from "./Components/CuriosidadesPage/CuriosidadesPage";
 import InsetoDetails from "./Components/InsetoDetails/InsetoDetails";
 import LoginPage from "./Components/LoginPage/LoginPage";
+import CharacteristcPage from "./Components/CharacteristcPage/CharacteristcPage";
 
 const App = () => {
-  useEffect(() => {
-    axios.get("https://api-museu-entomologiaufra.herokuapp.com/abdomens").then((response) => {
-      console.log(response.data);
-      setData(response.data);
-    });
+  // useEffect(() => {
+  //   axios.get("https://api-museu-entomologiaufra.herokuapp.com/abdomens").then((response) => {
+  //     console.log(response.data);
+  //     setData(response.data);
+  //   });
 
-    console.log(data)
+  //   console.log(data)
 
-  }, []);
+  // }, []);
 
   // value from user
   const [inputValue, setInputValue] = useState("");
 
   //data from backend
-  const [data, setData] = useState([]);
+  const [data, setData] = useState([
+    {
+      nomeVulgar: "Louva Deus",
+      abdomen: "abdomen louva Deus",
+      asa: "asa louva Deus",
+      perna: "perna louva Deus",
+      aparelhoBucal: "aparelho bucal louva Deus",
+      antena: "antena louva Deus",
+      metamorfose: "metamorfose louva Deus",
+      comportamento: "comportamento louva Deus",
+      habitat: "habitat louva Deus",
+      filo: "filo louva Deus",
+      classe: "Insecta louva Deus",
+      ordem: "ordem louva Deus",
+      familia: "familia louva Deus",
+      genero: "genero louva Deus",
+      curiosidade: "é um inseto que curte louvar a Deus",
+    },
+    {
+      nomeVulgar: "Besourinho",
+      abdomen: "abdomen Besourinho",
+      asa: "asa Besourinho",
+      perna: "perna Besourinho",
+      aparelhoBucal: "aparelho bucal Besourinho",
+      antena: "antena Besourinho",
+      metamorfose: "metamorfose Besourinho",
+      comportamento: "comportamento Besourinho",
+      habitat: "habitat Besourinho",
+      filo: "filo Besourinho",
+      classe: "Insecta Besourinho",
+      ordem: "ordem Besourinho",
+      familia: "familia Besourinho",
+      genero: "genero Besourinho",
+      curiosidade:
+        "inseto de boa com a vida que curte batalhar com outros insetos",
+    },
+    {
+      nomeVulgar: "Cavalo do Cao",
+      abdomen: "abdomen Cavalo do Cao",
+      asa: "asa Cavalo do Cao",
+      perna: "perna Cavalo do Cao",
+      aparelhoBucal: "aparelho bucal Cavalo do Cao",
+      antena: "antena Cavalo do Cao",
+      metamorfose: "metamorfose Cavalo do Cao",
+      comportamento: "comportamento Cavalo do Cao",
+      habitat: "habitat Cavalo do Cao",
+      filo: "filo Cavalo do Cao",
+      classe: "Insecta Cavalo do Cao",
+      ordem: "ordem Cavalo do Cao",
+      familia: "familia Cavalo do Cao",
+      genero: "genero Cavalo do Cao",
+      curiosidade:
+        "inseto desgraçado, não é atoa que o nome é cavalo do cão, capaz de matar um boi",
+    },
+  ]);
 
 
   //catch the value from the form that contain the input user
@@ -53,9 +108,7 @@ const App = () => {
       })} */}
       
       <Routes>
-        <Route
-          path="/"
-          element={
+        <Route path="/" element={
             <HomePage
               inputValue={inputValue}
               handleSubmit={handleSubmit}
@@ -67,22 +120,24 @@ const App = () => {
         />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/admin" element={<AdminHomePage />} />
-        <Route
-          path="/curiosidades"
-          element={<CuriosidadesPage data={data} />}
+        <Route path="/curiosidades" element={<CuriosidadesPage data={data} />}
         />
         <Route
           path="*"
           element={
-            <>
-              {" "}
-              <h1>Página não encontrada</h1>{" "}
-            </>
+            <main>
+              <h1>Página não encontrada</h1>
+            </main>
           }
         />
-        <Route
+        {/* problem in the routes */}
+        {/* <Route
           path="/:nomeVulgarInseto"
           element={<InsetoDetails data={data} />}
+        /> */}
+        <Route
+          path="/:characteristPath"
+          element={<CharacteristcPage />}
         />
       </Routes>
     </BrowserRouter>
